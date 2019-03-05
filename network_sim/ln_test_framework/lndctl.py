ad2357f64290bd848b27d2644b4ff9a2ec0429b5
Sanket <sanket1729@gmail.com>
2019-03-05 17:43:01 -0600

    Addded address generatinon

---
+++ network_sim/ln_test_framework/lndctl.py
@@ -0,0 +1,13 @@
+# controller for bitcoind
+def regtest_context():
+	context = {}
+	context['cmd'] = "./ln-cli --network=regtest --no-macaroons "
+	return context
+
+def get_context(chain):
+	if chain == 'regtest':
+		return regtest_context()
+
+def getnewaddress(chain='regtest'):
+	context = get_context(chain)
+	return context['cmd'] + " newaddress np2wkh" 
