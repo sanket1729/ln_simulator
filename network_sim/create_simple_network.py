39c4205a84a2cc8fd297c0d14b3a97ad12a85a1d
Sanket <sanket1729@gmail.com>
2019-03-05 20:01:45 -0600

    Merge conflict

--- network_sim/create_simple_network.py
+++ network_sim/create_simple_network.py
@@ -6,8 +6,9 @@ import json
 from ln_test_framework.nodecontainer import *
 from ln_test_framework.bitcoindctl import *
 from ln_test_framework.lndctl import *
+from ln_test_framework.lnnetwork import *
 
-def main(n):
+def setup(n):
 	client = docker.from_env()
 	low_level_client = docker.APIClient()
 
@@ -17,13 +18,17 @@ def main(n):
 	print("waiting 5 sec for bitcoind to start")
 	time.sleep(5)
 	print("Mining 150 blocks")
-	x = bitcoind_container.exec_run(generatetoaddress(150))
-	print(x)
+	bitcoind_container.exec_run(generatetoaddress(150))
+	# print(x)
+	
+
 	# 2) Create n instances of lnd
 	lnd_containers = []
 	print("Starting lnd containers")
 	for i in range(0, n):
 		lnd_containers.append(client.containers.run('lnd', name = "ln_node-" + str(i),detach=True))
+	
+
 	# 3) Get IP address and construct nodes for these containers
 	
 	# get bitcoind container info
@@ -42,16 +47,16 @@ def main(n):
 		lnd_node_ip = low_level_client.inspect_container(lnd_containers[i].id)['NetworkSettings']['IPAddress']
 		lnd_nodes.append(LndNode(ip_address = lnd_node_ip, node_id = lnd_containers[i].id, container = lnd_containers[i], pubkey = pubkey))
 	
+	#Print all info sanity
 	print(bitcoind_node.node_id, bitcoind_node.ip_address)
 	for i in range(0, n):
-		print(lnd_nodes[i].node_id, lnd_nodes[i].ip_address)
+		print(lnd_nodes[i].node_id, lnd_nodes[i].ip_address, lnd_nodes[i].pubkey)
 
-	temp = lnd_containers[0].exec_run(getnewaddress())
-	print(temp)
-	return
+	net = LNnetwork(bitcoind_node, lnd_nodes)
+	return net
 
 if __name__ == "__main__":
 	n =2
 	if len(sys.argv) == 2:
 		n = sys.argv[1] 
-	main(n)	
+	setup(n)	
---
+++ network_sim/ln_test_framework/lnnetwork.py
@@ -0,0 +1,4 @@
+class LNnetwork:
+	def __init__(self, bitcoind_node, lnd_nodes):
+		self.bitcoind_node = bitcoind_node
+		self.lnd_nodes = lnd_nodes
